6761a96ed6478e85aa572b615ab1f5cdddeea2f9
diego vega <da-vce@hotmail.com>
2026-05-07 13:24:58 -0500

    feat: Add option to use as Link in Button component

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,5 +1,6 @@
 import { cva } from "class-variance-authority";
 import type { VariantProps } from "class-variance-authority";
+import Link from "next/link";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center hover:cursor-pointer rounded-md",
@@ -9,10 +10,12 @@ const buttonVariants = cva(
         default:
           "bg-foreground text-background hover:bg-muted-foreground hover:text-background",
         ghost: "bg-none hover:bg-highlight",
+        withIcon: "justify-start gap-2 bg-none hover:bg-highlight",
       },
       size: {
-        default: "h-8 px-4 py-2",
+        default: "h-8 px-2",
         lg: "h-10 px-4 py-2",
+        md: "h-8 px-4 py-2",
       },
       disabled: {
         false: null,
@@ -31,12 +34,15 @@ export default function Button({
   variant,
   size,
   className,
+  icon: Icon,
   ...props
-}: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants>) {
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & { icon?: React.ComponentType }) {
+  const Component = props.href ? Link : "button";
   return (
-    <button
-      className={buttonVariants({ variant, size, className })}
-      {...props}
-    />
+    <Component className={buttonVariants({ variant, size, className })} {...props}>
+      {Icon && <Icon />}
+      {props.children}
+    </Component>
   );
 }
