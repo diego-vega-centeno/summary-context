d874f3eefc60a05e82f530a4a6177e0fc2c891e9
diego vega <da-vce@hotmail.com>
2026-05-07 21:02:22 -0500

    feat: add StatusCard component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen">
       <div className="md:w-64"><SideNav/></div>
-      <div className="flex-1">{children}</div>
+      <div className="flex-1 min-w-sm">{children}</div>
     </div>
   );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,69 @@
+import { getPRsByStatus, dummyPRs } from "@/lib/data/dummy-data";
+import {
+  GitPullRequest,
+  GitMerge,
+  SquareDot,
+  TriangleAlert,
+} from "lucide-react";
+
+const total_prs_length = dummyPRs.length;
+
+const status_config = {
+  total: { title: "total", icon: GitPullRequest, color: "text-grey-700" },
+  open: { title: "open", icon: GitPullRequest, color: "text-green-700" },
+  merged: { title: "merged", icon: GitMerge, color: "text-purple-700" },
+  closed: { title: "closed", icon: SquareDot, color: "text-grey-700" },
+  stale: { title: "stale", icon: TriangleAlert, color: "text-yellow-700" },
+};
+
+function StatsCard({
+  value,
+  config,
+}: {
+  value: number;
+  config: Record<string, any>;
+}) {
+  return (
+    <div
+      className={`p-3 border-1 border-border rounded-lg flex items-center gap-2`}
+    >
+      <div className="w-2/3">
+        <div className="text-lg text-muted-foreground">{config.title}</div>
+        <div className="text-2xl">{value}</div>
+      </div>
+      <div
+        className={`w-10 h-10 flex items-center justify-center text-2xl ${config.color} bg-muted-background rounded-xl`}
+      >
+        {<config.icon />}
+      </div>
+    </div>
+  );
+}
+
 export default function Page() {
   return (
-    <main className="flex flex-1 h-full w-full flex-col items-center justify-between p-4 sm:items-start">
-      <div className="text-center sm:items-start sm:text-left">
+    <main className="flex flex-1 h-full w-full flex-col justify-between p-6 max-w-6xl mx-auto">
+      <div className="">
         <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight">
           Dashboard
         </h1>
-        <h2 className="text-muted-foreground">Last synced</h2>
-        <p className="max-w-md text-lg leading-8 text-zinc-600">cards</p>
+        <h2 className="text-muted-foreground p-4 pl-0">Last synced</h2>
+        <div className="grid md:grid-cols-[repeat(auto-fit,minmax(120px,1fr))] grid-cols-2 gap-2">
+          {(
+            Object.keys(status_config) as Array<keyof typeof status_config>
+          ).map((type) => (
+            <div key={type}>
+              <StatsCard
+                value={
+                  type === "total"
+                    ? total_prs_length
+                    : getPRsByStatus(type).length
+                }
+                config={status_config[type]}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
