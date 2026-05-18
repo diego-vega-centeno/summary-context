b8e525f099408e665593584db3f06cfe67ab3175
diego vega <da-vce@hotmail.com>
2026-05-18 17:06:43 -0500

    feat: use Suspense and remove unused skeleton

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,7 +1,5 @@
 import { Suspense } from "react";
-import PRMiniCard from "@/components/ui/dashboard/PRMiniCard";
 import DashboardCount from "@/components/ui/dashboard/DashboardCount";
-import PRMiniCardSkeleton from "@/components/ui/dashboard/PRMiniCardSkeleton";
 import DashboardStatus from "@/components/ui/dashboard/DashboardStatus";
 import { DashboardStatusSkeleton } from "@/components/ui/skeletons";
 
@@ -10,10 +8,9 @@ export default async function DashboardPage() {
     <main className="flex flex-1 flex-col justify-between p-6 max-w-6xl mx-auto">
       <div>
         <DashboardCount />
-        {/* <Suspense fallback={<DashboardStatusSkeleton />}>
+        <Suspense fallback={<DashboardStatusSkeleton />}>
           <DashboardStatus />
-        </Suspense> */}
-        <DashboardStatusSkeleton />
+        </Suspense>
       </div>
     </main>
   );
