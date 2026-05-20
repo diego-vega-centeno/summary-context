c8cf94ed9888ab69903710d8dcf6b42aec2b62b3
diego vega <da-vce@hotmail.com>
2026-05-19 20:58:07 -0500

    feat: add StoriesClientPageSkeleton

--- app/(app)/stories/page.tsx
+++ app/(app)/stories/page.tsx
@@ -1,10 +1,21 @@
 import { fetchDashboardPRs } from "@/lib/data/prs";
 import StoriesClientPage from "@/app/ui/stories/StoriesClientPage";
-
+import { Suspense } from "react";
+import { StoriesClientPageSkeleton } from "@/app/ui/skeletons";
 
 export default async function StoriesPage() {
+  return (
+    <div>
+      {/* <StoriesClientPageSkeleton /> */}
+      <Suspense fallback={<StoriesClientPageSkeleton />}>
+        <StoriesClientPageWrapper />
+      </Suspense>
+    </div>
+  );
+}
+
+async function StoriesClientPageWrapper() {
   const userId = "7f759600-988e-4a81-9878-439523293021";
   const dashboardPRs = await fetchDashboardPRs(userId);
-
   return <StoriesClientPage initialPrs={dashboardPRs} />;
 }
--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,8 +1,9 @@
 import { statusConfig } from "@/lib/data/status-data";
 import { PRStatus } from "@/types";
 import SyncButton from "./SyncButton";
+import { Plus } from "lucide-react";
 
-const columns: PRStatus[] = ["open", "stale", "merged", "closed"];
+const status: PRStatus[] = ["open", "stale", "merged", "closed"];
 const shimmer =
   "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-gray-500/20 before:to-transparent";
 
@@ -16,7 +17,7 @@ export function DashboardStatusSkeleton() {
         click to view full story
       </div>
       <div className="grid md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] grid-cols-2 gap-2">
-        {columns.map((status) => (
+        {status.map((status) => (
           <div className={"flex flex-col gap-2"} key={status}>
             <div className="py-2">
               <div
@@ -56,7 +57,7 @@ export function DashboardCountSkeleton() {
       <div
         className={` grid md:grid-cols-[repeat(auto-fit,minmax(170px,1fr))] grid-cols-2 gap-2`}
       >
-        {["total", ...columns].map((status) => {
+        {["total", ...status].map((status) => {
           return (
             <div
               key={status}
@@ -66,5 +67,63 @@ export function DashboardCountSkeleton() {
         })}
       </div>
     </>
+  );
+}
+
+export function StoriesClientPageSkeleton() {
+  return (
+    <main className="flex flex-1 flex-col justify-between p-6 px-20 max-w-6xl mx-auto">
+      <div>
+        <div className="flex justify-between items-center mb-3">
+          <div>
+            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight">
+              Stories
+            </h1>
+            <h2 className="text-muted-foreground text-sm">Last synced</h2>
+          </div>
+          <button
+            type="button"
+            className={`inline-flex items-center justify-center rounded-md hover:bg-highlight hover:text-foreground h-7 px-2 border-1 border-border bg-foreground text-background`}
+          >
+            <Plus className="w-5 h-5 mr-1" />
+            Add PR
+          </button>
+        </div>
+        <div className="w-full flex gap-3 mb-8 flex-wrap">
+          <input
+            placeholder="Search PRs ..."
+            className="min-w-full md:min-w-40 w-3/8 bg-sidebar-background p-2 rounded-md text-sm"
+            type="text"
+          />
+          {["total", ...status].map((status) => (
+            <div
+              className="min-h-9 flex items-center gap-2 px-2 rounded-xl text-sm hover:cursor-pointer border-1 border-border font-semibold"
+              key={status}
+            >
+              <span>{status}</span>
+              <div className="inline-flex items-center justify-center rounded-full bg-muted-background w-6 h-6 text-white text-xs ml-2">
+                0
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-2">
+          <div className={"flex flex-col gap-2"}>
+            {Array.from({ length: 2 }, () => (
+              <div
+                className={`${shimmer} relative overflow-hidden w-full h-[200px] border-1 border-border rounded-lg`}
+              />
+            ))}
+          </div>
+          <div className={"flex flex-col gap-2"}>
+            {Array.from({ length: 2 }, () => (
+              <div
+                className={`${shimmer} relative overflow-hidden w-full h-[200px] border-1 border-border rounded-lg`}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
